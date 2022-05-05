0474c06564f19362976e1b1049901ff6c73b5a77
Maxence Pelletier-Lebrun <maxence.pelletier-lebrun.1@ulaval.ca>
2022-05-05 13:56:59 -0400

    Add square signal generator

---
+++ signal_generator.py
@@ -0,0 +1,7 @@
+from scipy import signal
+import numpy as np
+import matplotlib.pyplot as plt
+
+def square_signal(time_values, frequency, signal_width):
+    duty_cycle = 1 - signal_width*frequency
+    return np.array(signal.square(2 * np.pi * frequency * time_values, duty_cycle)).clip(min=0)
