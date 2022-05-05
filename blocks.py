a2132c65d313cbb8e77ad3552a1d041a0457a330
Maxence Pelletier-Lebrun <maxence.pelletier-lebrun.1@ulaval.ca>
2022-05-05 16:24:47 -0400

    Add semi-random square function generator Generates a semi-random square function over a given time interval. Makes a determined number of pulses of semi-random center with jitter parameter.

--- blocks.py
+++ blocks.py
@@ -1,15 +1,25 @@
 import time
 import random
+import numpy as np
+from signal_generator import generate_signal
+import matplotlib.pyplot as plt
 
 class stimulation:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, duration, width, pulses=0, jitter=0, frequency=0, type='square'):
+        self.duration = duration
+        self.pulses = pulses
+        self.width = width
+        self.jitter = jitter
+        self.frequency = frequency
+        self.time = np.linspace(0, duration, duration*1000)
+        self.signal = generate_signal(self.time, type, width, pulses, jitter, frequency)
 
     def __str__(self):
         return str(self.data)
 
     def run(self):
-        print(self.data)
+        plt.plot(self.time, self.signal)
+        plt.show()
 
 
 
--- signal_generator.py
+++ signal_generator.py
@@ -1,7 +1,24 @@
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 def square_signal(time_values, frequency, signal_width):
     duty_cycle = 1 - signal_width * frequency
     return np.array(signal.square(2 * np.pi * frequency * time_values, duty_cycle)).clip(min=0)
+
+def random_square(time_values, pulses, width, jitter):
+    signal = np.zeros(len(time_values))
+    buffer = (float(width)+jitter, float(time_values[-1]-width-jitter))
+    uniform_distribution = np.linspace(*buffer, pulses)
+    random_numbers = np.around(np.random.uniform(-jitter, jitter, pulses), 3)
+    randomized_distribution = uniform_distribution + random_numbers
+    for value in randomized_distribution:
+        signal[(time_values>value-width/2) & (time_values<value+width/2)] = 5
+    return signal
+
+def generate_signal(time, type, width, pulses, jitter, frequency):
+    if type == 'square':
+        square_signal(time, frequency, width)
+    if type == 'random-square':
+        random_square(time, pulses, width, jitter)
