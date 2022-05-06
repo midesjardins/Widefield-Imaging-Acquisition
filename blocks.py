3ec04e6d363431668184dacc34482c3c3b4e0723
Maxence Pelletier-Lebrun <maxence.pelletier-lebrun.1@ulaval.ca>
2022-05-06 11:19:20 -0400

    Introduce smart delay between stimulations The last stimulation is configured not to have any delay at its end. The "blocks" delay will act as the delay between distincts stimulations. Delay is added directly to the signal, which will permit maintained camera monitoring even while the mouse is not stimulated.

--- blocks.py
+++ blocks.py
@@ -5,33 +5,57 @@ from signal_generator import generate_signal
 import matplotlib.pyplot as plt
 
 class stimulation:
-    def __init__(self, duration, width, pulses=0, jitter=0, frequency=0, type='square'):
+    def __init__(self, duration, width, pulses=0, jitter=0, frequency=0, delay=0, type='square'):
         self.duration = duration
+        self.type = type
+        self.delay = delay
         self.pulses = pulses
         self.width = width
         self.jitter = jitter
         self.frequency = frequency
+        self.time_delay = np.linspace(0, delay, delay*1000)
         self.time = np.linspace(0, duration, duration*1000)
-        self.signal = generate_signal(self.time, type, width, pulses, jitter, frequency)
-
+        self.empty_signal = np.zeros(len(self.time_delay))
     def __str__(self):
         return str(self.data)
+    
+    def randomize_signal(self):
+        self.signal = generate_signal(self.time, self.type, self.width, self.pulses, self.jitter, self.frequency)
 
-    def run(self):
-        plt.plot(self.time, self.signal)
+    def run(self, last=False):
+        self.randomize_signal()
+        if last is False:
+            plt.plot(np.concatenate((self.time,self.time_delay + self.duration)), np.concatenate((self.signal, self.empty_signal)))
+        else:
+            plt.plot(self.time, self.signal)
+
         plt.show()
+        plt.clf()
 
 
 
 class blocks:
-    def __init__(self, data, delay=0, iterations=1, jitter=0):
+    def __init__(self, data, delay=0, iterations=1):
         self.data = data
         self.iterations = iterations
         self.delay = delay
-        self.jitter = jitter
 
-    def run(self):
-        for item in self.data:
-                item.run()
-                print(self.delay + round(random.random(), 2) * self.jitter)
-                time.sleep(self.delay + round(random.random(), 2) * self.jitter)
+    def run(self, last=False):
+        for iteration in range(self.iterations):
+            for index, item in enumerate(self.data):
+                print(f'iteration={iteration}')
+                print(f'index={index}')
+                print(f'self.iterations={self.iterations}')
+                if iteration + 1 != self.iterations:
+                    item.run()
+                else:
+                    item.run(last=True)
+            if iteration + 1 != self.iterations:
+                time.sleep(self.delay)
+
+a = stimulation(100, 4, 4, 1, delay=200, type='random-square')
+b = stimulation(200, 4, 8, 1, delay=50, type='random-square')
+c = blocks([a], iterations=2)
+d = blocks([b], iterations=3)
+e = blocks([c,d], delay=2, iterations=2)
+e.run()
