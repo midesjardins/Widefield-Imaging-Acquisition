deab0e0598d4bc5fc0542ad10ea7c831e4609cfc
maxenceplebrun <58450881+maxenceplebrun@users.noreply.github.com>
2022-05-18 16:14:33 -0400

    Add working stimulation tree and improve interface

--- gui/interface.py
+++ gui/interface.py
@@ -2,14 +2,14 @@
 from curses.panel import top_panel
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QModelIndex
 from PyQt5.QtGui import *
 
 class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 file system view - pythonspot.com'
+        self.title = 'Widefield Imaging Aquisition'
         self.left = 10
         self.top = 10
         self.width = 640
@@ -20,12 +20,6 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         
-        self.model = QFileSystemModel()
-        self.model.setRootPath('')
-        self.tree = QTreeWidget()
-        self.item = QTreeWidgetItem()
-        self.item.setText(0,"Test")
-        self.tree.addTopLevelItem(self.item)
         self.grid_layout = QGridLayout()
         self.setLayout(self.grid_layout)
         self.grid_layout.setAlignment(Qt.AlignTop)
@@ -51,8 +45,13 @@ class App(QWidget):
         self.directory_window = QHBoxLayout()
         self.directory_label = QLabel('Directory')
         self.directory_window.addWidget(self.directory_label)
-        self.directory_cell = QLineEdit()
+        self.directory_cell = QLineEdit("")
+        self.directory_cell.setReadOnly(True)
         self.directory_window.addWidget(self.directory_cell)
+        self.directory_choose_button = QPushButton("Choose")
+        self.directory_choose_button.setIcon(QIcon("gui/icons/folder-plus.png"))
+        self.directory_choose_button.clicked.connect(self.choose_directory)
+        self.directory_window.addWidget(self.directory_choose_button)
         self.experiment_settings_main_window.addLayout(self.directory_window)
 
         self.grid_layout.addLayout(self.experiment_settings_main_window, 1, 0)
@@ -77,11 +76,13 @@ class App(QWidget):
         self.image_settings_main_window.addLayout(self.exposure_window)
 
         self.image_settings_second_window = QHBoxLayout()
-        self.speckle_button = QCheckBox('Speckle')
+        self.speckle_button = QCheckBox('Infrared')
         self.image_settings_second_window.addWidget(self.speckle_button)
-        self.intrinsic_button = QCheckBox('Intrinsic')
-        self.image_settings_second_window.addWidget(self.intrinsic_button)
-        self.fluorescence_button = QCheckBox('Fluorescence')
+        self.red_button = QCheckBox('Red')
+        self.image_settings_second_window.addWidget(self.red_button)
+        self.green_button = QCheckBox('Green')
+        self.image_settings_second_window.addWidget(self.green_button)
+        self.fluorescence_button = QCheckBox('Blue')
         self.image_settings_second_window.addWidget(self.fluorescence_button)
         self.image_settings_main_window.addLayout(self.image_settings_second_window)
         
@@ -100,6 +101,37 @@ class App(QWidget):
         self.stimulation_tree_label = QLabel('Stimulation Tree')
         self.grid_layout.addWidget(self.stimulation_tree_label, 2, 0)
 
+        self.stimulation_tree_window = QVBoxLayout()
+        self.stimulation_tree = QTreeWidget()
+        self.stimulation_tree.currentItemChanged.connect(self.actualize_tree)
+        self.stimulation_tree_window.addWidget(self.stimulation_tree)
+
+
+        self.stimulation_tree_switch_window = QStackedLayout()
+        self.stimulation_tree_second_window = QHBoxLayout()
+        self.stim_buttons_container = QWidget()
+        self.delete_branch_button = QPushButton('Delete')
+        self.delete_branch_button.setIcon(QIcon("gui/icons/trash.png"))
+        self.delete_branch_button.clicked.connect(self.delete_branch)
+        self.stimulation_tree_second_window.addWidget(self.delete_branch_button)
+        self.add_brother_branch_button = QPushButton('Add Sibling')
+        self.add_brother_branch_button.clicked.connect(self.add_brother)
+        self.add_brother_branch_button.setIcon(QIcon("gui/icons/arrow-bar-down.png"))
+        self.stimulation_tree_second_window.addWidget(self.add_brother_branch_button)
+        self.add_child_branch_button = QPushButton('Add Child')
+        self.add_child_branch_button.clicked.connect(self.add_child)
+        self.add_child_branch_button.setIcon(QIcon("gui/icons/arrow-bar-right.png"))
+        self.stimulation_tree_second_window.addWidget(self.add_child_branch_button)
+        self.stim_buttons_container.setLayout(self.stimulation_tree_second_window)
+        self.stimulation_tree_switch_window.addWidget(self.stim_buttons_container)
+        self.new_branch_button = QPushButton("New Stimulation")
+        self.new_branch_button.clicked.connect(self.first_stimulation)
+        self.stimulation_tree_switch_window.addWidget(self.new_branch_button)
+        
+        self.stimulation_tree_window.addLayout(self.stimulation_tree_switch_window)
+        self.stimulation_tree_switch_window.setCurrentIndex(1)
+        self.grid_layout.addLayout(self.stimulation_tree_window, 3, 0)
+
         self.signal_adjust_label = QLabel('Signal Adjust')
         self.grid_layout.addWidget(self.signal_adjust_label, 2, 1)
 
@@ -108,12 +140,14 @@ class App(QWidget):
 
         self.buttons_main_window = QHBoxLayout()
         self.stop_button = QPushButton('Stop')
+        self.stop_button.setIcon(QIcon("gui/icons/player-stop.png"))
         self.stop_button.clicked.connect(self.stop)
         self.buttons_main_window.addWidget(self.stop_button)
         self.run_button = QPushButton('Run')
+        self.run_button.setIcon(QIcon("gui/icons/player-play.png"))
         self.run_button.clicked.connect(self.run)
         self.buttons_main_window.addWidget(self.run_button)
-        self.grid_layout.addLayout(self.buttons_main_window, 3, 2)
+        self.grid_layout.addLayout(self.buttons_main_window, 4, 2)
 
         self.show()
 
@@ -121,8 +155,9 @@ class App(QWidget):
         lights = []
         if self.speckle_button.isChecked():
             lights.append('ir')
-        if self.intrinsic_button.isChecked():
+        if self.red_button.isChecked():
             lights.append('red')
+        if self.green_button.isChecked():
             lights.append('green')
         if self.fluorescence_button.isChecked():
             lights.append('blue')
@@ -130,6 +165,61 @@ class App(QWidget):
 
     def stop(self):
         pass
+
+    def choose_directory(self):
+        folder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.directory_cell.setText(folder)
+
+    def delete_branch(self):
+        root = self.stimulation_tree.invisibleRootItem()
+        parent = self.stimulation_tree.currentItem().parent()
+        try:
+            if parent.childCount() == 1:
+                parent.setIcon(0, QIcon("gui/icons/wave-square.png"))
+        except Exception:
+            pass
+        (parent or root).removeChild(self.stimulation_tree.currentItem())
+    
+    def add_brother(self):
+        if self.stimulation_tree.currentItem():
+            stimulation_tree_item = QTreeWidgetItem()
+            stimulation_tree_item.setText(0, "Test5")
+            stimulation_tree_item.setIcon(0, QIcon("gui/icons/wave-square.png"))
+            #stimulation_tree_item.setIcon(0, self.style().standardIcon(getattr(QStyle, "SP_DirIcon")))
+            parent = self.stimulation_tree.selectedItems()[0].parent()
+            if parent:
+                index = parent.indexOfChild(self.stimulation_tree.selectedItems()[0])
+                parent.insertChild(index+1, stimulation_tree_item)
+            else:
+                self.stimulation_tree.addTopLevelItem(stimulation_tree_item)
+        else:
+            pass
+
+    def add_child(self):
+        if self.stimulation_tree.currentItem():
+            self.stimulation_tree.currentItem().setIcon(0, QIcon("gui/icons/package.png"))
+            stimulation_tree_item = QTreeWidgetItem()
+            stimulation_tree_item.setIcon(0, QIcon("gui/icons/wave-square.png"))
+            stimulation_tree_item.setText(0,"Test4")
+            self.stimulation_tree.selectedItems()[0].addChild(stimulation_tree_item)
+            self.stimulation_tree.selectedItems()[0].setExpanded(True)
+        else:
+            pass
+
+    def actualize_tree(self):
+        if self.stimulation_tree.currentItem():
+            self.stimulation_tree_switch_window.setCurrentIndex(0)
+        else:
+            self.stimulation_tree_switch_window.setCurrentIndex(1)
+
+    def first_stimulation(self):
+        stimulation_tree_item = QTreeWidgetItem()
+        stimulation_tree_item.setIcon(0, QIcon("gui/icons/wave-square.png"))
+        stimulation_tree_item.setText(0, "First Item")
+        self.stimulation_tree.addTopLevelItem(stimulation_tree_item)
+        self.stimulation_tree_switch_window.setCurrentIndex(0)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
