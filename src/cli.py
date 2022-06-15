09147a8531d53183d63e1c7d5c2537de1ad65282
Maxence Pelletier-Lebrun <maxence382@icloud.com>
2022-06-15 12:04:10 -0400

    Add working CLI

--- src/cli.py
+++ src/cli.py
@@ -4,10 +4,10 @@ import numpy as np
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from src.data_handling import extract_from_path, separate_images
 
-path = sys.argv[0]
+path = sys.argv[1]
 lights, frames = extract_from_path(path)
 arrays = separate_images(lights, frames)
 for i, array in enumerate(arrays):
-    print(f"Setting {lights[i].capitalize()} Light...")
+    print(f"Saving {lights[i].capitalize()} Channel...")
     np.save(os.path.join(path, f"{lights[i]}.npy"), array)
 print("Extraction Done!")
